bb58a632f037223a4942546af5efcb50579a47b7
Miłosz Linkiewicz <milosz.linkiewicz@intel.com>
2025-05-13 18:26:05 +0200

    ADD: Validation tests and image optimizations for RAISR (#10)
    ADD: Validation tests for RAISR
    ADD: Runtime stage with proper labeling
    FIX: Image size issues
    FIX: Docker registry login and push
    FIX: Filters scripting in tests/validation/create_wrong_files.sh
    
    ---------
    
    Signed-off-by: Milosz Linkiewicz <milosz.linkiewicz@intel.com>

--- tests/validation/create_wrong_files.sh
+++ tests/validation/create_wrong_files.sh
@@ -3,16 +3,15 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright 2024-2025 Intel Corporation
 
-SCRIPT_DIR="$(readlink -f "$(dirname -- "${BASH_SOURCE[0]}")")"
-REPO_DIR="$(readlink -f "${SCRIPT_DIR}/../..")"
+REPO_DIR="$(readlink -f "/opt/intel_ai_suite")"
 
-ffmpeg_path="${ffmpeg_path:-${REPO_DIR}/raisr/ffmpeg}"
+ffmpeg_path="${ffmpeg_path:-${REPO_DIR}/}"
 out_filters=(filters_badNums  filters_wrongConfig filters_noPathes)
 bad_configs=(12_3_3_11 24_3_3 24_3_3_6 24_3_3_9)
 bad_nums=(bad_cohpath_nums bad_hashtable_nums bad_strpath_nums)
 no_pathes=(noCohPath noConfig noHashTable noStrPath)
 
-for outfilter in ${out_filters[@]}; do
+for outfilter in "${out_filters[@]}"; do
     echo $outfilter
     if [ -e $outfilter ]
     then
@@ -21,10 +20,10 @@ for outfilter in ${out_filters[@]}; do
     mkdir -p "$ffmpeg_path/$outfilter"
     case "$outfilter" in
         "filters_badNums")
-            for bad_num in ${bad_nums[@]}; do
+            for bad_num in "${bad_nums[@]}"; do
                 mild_path=filters1_$bad_num
-                cp -r ${REPO_DIR}/filters_2x/filters_highres "$ffmpeg_path/$outfilter/$mild_path"
-                cd "$ffmpeg_path/$outfilter/$mild_path"
+                cp -r /filters_2x/filters_highres "$ffmpeg_path/$outfilter/$mild_path"
+                cd "$ffmpeg_path/$outfilter/$mild_path" || exit 10
                 case "$mild_path" in
                     "filters1_bad_cohpath_nums")
                         mv Qfactor_cohbin_2_8 Qfactor_cohbin_6_8
@@ -42,29 +41,28 @@ for outfilter in ${out_filters[@]}; do
                         mv Qfactor_strbin_2_10 Qfactor_strbin_6_10
                     ;;
                 esac
-                cd "$OLDPWD"
+                cd "$OLDPWD" || exit 10
             done
         ;;
         "filters_wrongConfig")
-            for bad_conf in ${bad_configs[@]}; do
+            for bad_conf in "${bad_configs[@]}"; do
                 mild_path=filters1_$bad_conf
-                cp -r ${REPO_DIR}/filters_2x/filters_highres "$ffmpeg_path/$outfilter/$mild_path"
-                cd "$ffmpeg_path/$outfilter/$mild_path"
+                cp -r /filters_2x/filters_highres "$ffmpeg_path/$outfilter/$mild_path"
+                cd "$ffmpeg_path/$outfilter/$mild_path" || exit 10
                 confignum=${bad_conf//_/ }
                 sed -i "1c$confignum" config
-                cd "$OLDPWD"
+                cd "$OLDPWD" || exit 10
             done
         ;;
         "filters_noPathes")
-            for bad_path in ${no_pathes[@]}; do
+            for bad_path in "${no_pathes[@]}"; do
                 mild_path=filters1_$bad_path
-                cp -r ${REPO_DIR}/filters_2x/filters_highres "$ffmpeg_path/$outfilter/$mild_path"
-                cd "$ffmpeg_path/$outfilter/$mild_path"
+                cp -r /filters_2x/filters_highres "$ffmpeg_path/$outfilter/$mild_path"
+                cd "$ffmpeg_path/$outfilter/$mild_path" || exit 10
                 echo $mild_path
                 type_Coh=$(echo $mild_path | grep "Coh")
                 type_Conf=$(echo $mild_path | grep "Conf")
                 type_Hash=$(echo $mild_path | grep "Hash")
-                type_Str=$(echo $mild_path | grep "Str")
                 if [[ "$type_Coh" != "" ]]
                 then
                     rm -rf Qfactor_cohbin_*
@@ -77,7 +75,7 @@ for outfilter in ${out_filters[@]}; do
                 else
                     rm -rf Qfactor_strbin_*
                 fi
-                cd "$OLDPWD"
+                cd "$OLDPWD" || exit 10
             done
         ;;
     esac
