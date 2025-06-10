71b5c327b4a35de0e49ee7909153c8dc6f00f8d0
Miłosz Linkiewicz <milosz.linkiewicz@intel.com>
2025-06-10 19:09:16 +0200

    ADD: GitHub actions for tests and validation (#15)
    MAJOR CHANGE: Added iVSR and RAIS smoke tests
    
    ADD: GitHub actions for tests and validation
    FIX: Minor dockerfile improvements
    
    ADD: GPG Encryption added and keys
    Added test cases for iVSR in encrypted mode
    
    Add coverity and more tests added to the secrets
    FIX: Trivy scans and SBOM generation in build_docker_tpl.yml
    FIX Update build_docker_tpl.yml fixed caching and file size
    FIX: Update trivy.yml generation
    ADD: GitHub actions for tests and validation
    FIX: Minor Dockerfile improvements

--- build_ivsr_raisr_docker.sh
+++ build_ivsr_raisr_docker.sh
@@ -2,16 +2,27 @@
 
 docker_tag="docker.io/ivsr_raisr:25.04-alpha"
 
-docker build --build-arg http_proxy="${http_proxy:-}" \
-  --build-arg https_proxy="${https_proxy:-}" \
-  --build-arg no_proxy="${no_proxy:-}" \
-  --build-arg PYTHON=python3.10 \
-  --build-arg ENABLE_OV_PATCH=false \
-  --build-arg OV_VERSION=2024.5 \
-  -f Dockerfile -t "${docker_tag}" --progress=plain \
-  ./
+function build_docker_image()
+{
+  docker build --build-arg http_proxy="${http_proxy:-}" \
+    --build-arg https_proxy="${https_proxy:-}" \
+    --build-arg no_proxy="${no_proxy:-}" \
+    --build-arg PYTHON=python3.10 \
+    --build-arg ENABLE_OV_PATCH=false \
+    --build-arg OV_VERSION=2024.5 \
+    -f Dockerfile -t "${docker_tag}" \
+    --target runtime-stage --progress=plain \
+    ./
+}
 
-# load ${docker_tag} image into crictl
-docker save -o ivsr_raisr.tar "${docker_tag}"
-sudo ctr -n k8s.io images import ivsr_raisr.tar
-rm ivsr_raisr.tar
+function export_to_ctr()
+{
+  # load ${docker_tag} image into crictl
+  docker save -o ivsr_raisr.tar "${docker_tag}"
+  sudo ctr -n k8s.io images import ivsr_raisr.tar
+  rm ivsr_raisr.tar
+}
+
+build_docker_image || exit 1
+[[ "${EXPORT_TO_CTR}" == "1" ]] && export_to_ctr
+
